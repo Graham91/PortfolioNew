fb133fa4f1aad938ddc04a8ec4d81bfedaa869b3
grambo <gramham@live.com>
2021-07-19 14:14:54 -0600

    got animations lined up

---
+++ loweranimations.js
@@ -0,0 +1,8 @@
+{"frameNumber":"24",
+"rotationY":"188",
+"boxLeftLocation":"55",
+"boxTopLocation":"56.8",
+"perspectiveAdjustment":"600",
+"WidthPercentage":"30.5",
+"HeightPercentage":0,
+"rotationX":0}
